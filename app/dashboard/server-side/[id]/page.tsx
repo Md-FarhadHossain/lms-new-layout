3a0aefbd58ce209b781ab7ddb84823f8f7f2a11a
Md-FarhadHossain <freelancerfarhadhossain@gmail.com>
2024-05-02 02:51:05 +0600

    all the iframe and the aside section is responsive now

--- app/dashboard/server-side/[id]/page.tsx
+++ app/dashboard/server-side/[id]/page.tsx
@@ -14,7 +14,7 @@ const ServerSideId = ({ params }: { params: server_side }) => {
 
   const url = `https://www.youtube.com/embed/${id}?rel=0&color=white`
   return (
-    <aside className='p-8 lg:h-[70vh] md:h-[60vh] lg:shadow-none h-full shadow-lg lg:rounded-none rounded-2xl'>
+    <aside className='p-8 lg:h-[70vh] md:h-[45vh] lg:shadow-none h-full shadow-lg lg:rounded-none rounded-2xl'>
 
 
       <iframe className='lg:h-[720px] lg:w-[full] w-full md:h-full h-[260px] sm:h-[400px] rounded-2xl' src={url} title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerPolicy="strict-origin-when-cross-origin" allowFullScreen></iframe>
